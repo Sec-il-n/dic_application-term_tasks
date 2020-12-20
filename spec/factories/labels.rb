6f531ddc2cc8648c378f8fd8a5628a14e9e33524
Sec-li-n <loveuallnbs0205@yahoo.com>
2020-12-20 09:19:25 +0900

    step5 提出前 コメント除去等

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,15 +1,12 @@
 FactoryBot.define do
-  sequence (:label_names) { |n| "label_#{n}" }
-  # sequence :label_names do |n|
-  #   "label_#{n}"
-  # end
+  i = 0
+  sequence (:label_names) { |n| "label_#{i += 1}" }
+  # sequence (:label_names) { |n| "label_#{n}" }
   factory :label do
     label_name { generate :label_names }
-    # { アソシエーション名.同? }
     association :user
     # undefined method　←先に要manager?
     # ActiveRecord::AssociationTypeMismatch:User(#70239587116260) expected, got インスタンス
-    # after(:create) do |label,user|
     after(:create) do |label|
     # after(:build) do |task_7|
       # メールアドレスはすでに存在します
@@ -17,13 +14,5 @@ FactoryBot.define do
       # labels: = label_ids: の意味(?) labelが実行されるごとに、idを[]格納
       # 3回実行されるtask_7にそれぞれ同一のlabel_idが入る。user側でlabelのcreate_listをしても、1のタスクに複数のラベルをつけられるわけではない。
     end
-    # after(:create) do |label|
-    #   task = create(:task_7)
-    #   create(:manager, label: label, task: task )
-    # end
-    # after(:build) do |label|　|label, user|　　
-    # 　　　　　　　　　　　　　　　　　　　　　　　⤵︎create  task{ user.tasks[].id }
-    # create(:manager, label: label, task: user.tasks[].id  )
-    # end
   end
 end
