7aab2ec566d23b974c3da417a97d60dbdbfd33fb
Sec-li-n <loveuallnbs0205@yahoo.com>
2020-12-21 07:22:43 +0900

    SystemSpec task_spec.rb 修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -24,13 +24,15 @@ FactoryBot.define do
     # 追記
     priority { "#{['高', '中', '低'].sample}" }
     # priority { "#{[1, 2, 3].sample}" }
+    association :user, factory: :user_1
   end
   factory :task_4, class: Task do
     task_name { generate :task_name_sequence }
     details { 'テスト_詳細3' }
-    created_at { "#{5.days.ago}" }
     valid_date { "#{Time.current.since(10.days)}"  }
+    status { "#{["未着手", "着手中", "完了"].sample}" }
     # valid_date { Time.current += 10 }
+    association :user, factory: :user_1
   end
   factory :task_5, class: Task do
     # task_name = [*'a'..'z'].shuffle[0..9]
@@ -39,7 +41,10 @@ FactoryBot.define do
     task_name { 'タスク' }
     details { 'テスト_詳細' }
     status { "#{["未着手", "着手中", "完了"].sample}" }
-    valid_date { '002022-12-18'  }
+    # valid_date { '002022-12-18'  }
+    valid_date { generate :task_valid_sequence }
+
+    association :user, factory: :user_1
   end
   factory :task_6, class: Task do
     task_name { 'タスク234' }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,34 @@ FactoryBot.define do
     password { "MyString" }
     password_confirmation { "MyString" }
   end
+  factory :user_1, class: User do
+    user_name { "MyString" }
+    email { "MyString@hoge.com" }
+    password { "MyString" }
+    password_confirmation { "MyString" }
+    # default
+    use_task_5
+    trait :use_task_5 do
+      after(:build) do |user|
+        create_list(:task_5, 5, user: user)
+      end
+    end
+    trait :use_task_4 do
+      after(:build) do |user|
+        create_list(:task_4, 5, user: user)
+      end
+    end
+    trait :use_task_6 do
+      after(:build) do |user|
+        create(:task_6, user: user)
+      end
+    end
+    trait :use_task_3 do
+      after(:build) do |user|
+        create(:task_3, user: user)
+      end
+    end
+  end
   factory :user_2, class: User do
     user_name { "MyString" }
     email { "MyString2@hoge.com" }
